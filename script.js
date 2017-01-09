7fddcf9477d1ead9093c775f8b49ecb930a69c49
Amaya <amayalocke@students.ptechnyc.org>
2017-01-09 15:05:31 +0000

    pixels project

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //Write your function calls below this line
+// setColor(0,0,"red");
+// setColor(0,9,"red");
+// setColor(9,0,"red");
+// setColor(9,9,"red");
+
+// setColor(3,2,"green");
+// setColor(3,6,"green");
+// setColor(5,0,"blue");
+// setColor(6,1,"red");
+// setColor(7,2,"purple");
+// setColor(7,3,"yellow");
+// setColor(7,4,"orange");
+// setColor(7,5,"green");
+// setColor(7,6,"blue");
+// setColor(6,7,"red");
+// setColor(5,8,"purple");
+
+setColor(0,3,"blue");
+setColor(0,4,"pink");
+setColor(1,2,"green");
+setColor(2,2,"purple");
+setColor(3,2,"orange");
+setColor(4,3,"red");
+setColor(4,4,"yellow");
+setColor(5,5,"pink");
+setColor(6,5,"blue");
+setColor(7,5,"green");
+setColor(8,4,"purple");
+setColor(9,3,"orange");
+setColor(9,2,"red");
